3884452628417ca923c4c3043eaaba9781700775
Fernando Mena <Fernando.mena.sanchez@gmail.com>
2018-05-28 10:52:46 -0700

    update for Get endpoing exercise

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 // we'll use morgan to log the HTTP layer
+const router = express.Router();
 const morgan = require('morgan');
 // we'll use body-parser's json() method to 
 // parse JSON data sent in requests to this app
@@ -8,7 +9,7 @@ const bodyParser = require('body-parser');
 
 // we import the ShoppingList model, which we'll
 // interact with in our GET endpoint
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -25,11 +26,19 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+
+Recipes.create('chocolate milk', ['cocoa', 'milk', 'sugar']);
+Recipes.create('potato salad', ['potatoes','mayonnaise', 'celery']);
+
 // when the root of this route is called with GET, return
 // all current ShoppingList items by calling `ShoppingList.get()`
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
+
+app.get('/recipes', (req,res) => {
+	res.json(Recipes.get());
+})
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
